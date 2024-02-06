5ca61db7bf76c6e1c3f20ddf98953427d8b2966e
Ewan-Reveille <ewan.reveille23@gmail.com>
2024-02-06 11:53:40 +0100

    app initialized

--- database/database.js
+++ database/database.js
@@ -1,14 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
-let sql;
 
-const db = new sqlite3.Database('./database/db.sqlite', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the database.');
+const db = new sqlite3.Database('./nicophoto.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the database.');
+    }
 });
 
-
-// Créez les tables dans la commande ci-dessous
-sql = ``;
-db.run(sql)
+module.exports = db;
