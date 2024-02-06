a409c50e0a4c6e5354b2c5a588a054aec5d5d66b
Ewan-Reveille <ewan.reveille23@gmail.com>
2024-02-06 16:40:36 +0100

    fixed index.routes.js

--- router/index.routes.js
+++ router/index.routes.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 
 // Importing routes
 
-const adminRoutes = require("./admin.routes");
+const adminRoutes = require("./auth.routes");
 const imageRoutes = require("./images.routes");
 const albumRoutes = require("./albums.routes");
 const authRoutes = require("./auth.routes");
