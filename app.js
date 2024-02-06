a4a241fa86f409018c7a5df1d43f12e2570efd24
xenotil <adrien.mamias@gmail.com>
2024-02-06 13:08:37 +0100

    Serve public

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.public("/public"));
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
