a3c21c973981ab203e57a0fef136cf9285192976
Nak0_x <pro.theolesage@gmail.com>
2024-02-09 08:57:19 +0100

    Backup for spliting back and front

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,29 +14,4 @@ function getAdminById(id){
   }
 }
 
-exports.authToken = (req, res, next)=>{
 
-    // Pickup the token into the headers
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1] // Bearer qlljmlqkjenkjlgqsg6q5erqe54qdhg
-
-    // Check if the token exsist
-    if(!token) return res.sendStatus(401)
-
-    // If it was an admin token
-    jwt.verify(token, process.env.ADMIN_TOKEN_SECRET, (err, admin)=>{
-        if(!err){
-            req.user = {...admin, admin: true}
-            next()
-        }else{
-
-            // Verify the token and go to the next middleware or req/res function
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
-                if(err) return res.sendStatus(401)
-                req.user = user
-                next();
-            })
-        }
-    })
-
-}
