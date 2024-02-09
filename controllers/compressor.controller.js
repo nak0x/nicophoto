a27f7ecfd67df60fef12ce36c4ec958d8887bdb5
Ewan-Reveille <ewan.reveille23@gmail.com>
2024-02-09 16:40:26 +0100

    compressor finished

--- controllers/compressor.controller.js
+++ controllers/compressor.controller.js
@@ -1,3 +1,21 @@
-const compressImage = (image) => {
-    
-}
+const sharp = require('sharp');
+const axios = require('axios');
+const fs = require('fs');
+
+const compressImage = async (imageUrl) => {
+    try {
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(response.data);
+
+        const compressedImageBuffer = await sharp(imageBuffer)
+            .resize({ height: 728 })
+            .toBuffer();
+
+        fs.writeFileSync('imageFinallyCompressed.webp', compressedImageBuffer);
+        console.log('Your image has been successfully compressed.');
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+compressImage('https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU');
