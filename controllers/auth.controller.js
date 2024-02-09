3a3c551ceec9d2a821902d3a7654da58834bcb5b
Nak0_x <pro.theolesage@gmail.com>
2024-02-09 16:38:52 +0100

    authToken routes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,6 @@
- /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/
 const jwt = require("jsonwebtoken");
-const joi = require("joi");
-const { Database } = require("../database/database");
+
+// Token managment
 
 exports.auth = ()=>{return}
-function getAdminById(id){
-  try{
-    const result = Database.run(`
-      SELECT * FROM admins WHERE login_id = ?;
-    `, [id]);
-    console.log(result);
-  }catch(err){
-    return {}
-  }
-}
+
--- validators/user.validator.js
+++ validators/user.validator.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 const Bcrypt = require("bcrypt");
-const { getCredentials } = require("../database/database_utils")
+const { getCredentials, getAdminCredentials } = require("../database/database_utils")
 
 // User access to album validation object
 const userValidation = Joi.object({
@@ -13,27 +13,51 @@ const userValidation = Joi.object({
         helper.error("any.invalid")
         return helper.message("The album seems to not exist")
       }
-      return true;
+      return albumId;
     }),
     pass: Joi
     .string()
     .required()
     .custom(async (encryptedPass, helper)=>{
       let credentials = await getCredentials(albumId);
-      if(credentials == {}){
-        helper.error("any.invalid")
-        return helper.message('Wrong password')
-      }
-      if(encryptedPass == credentials.pass){
+      if(credentials != {} && encryptedPass == credentials.pass) {
         return credentials.pass
       }
+      helper.message('Wrong password')
+      return helper.error("any.invalid")
+    })
+})
+
+// Admin access validation object
+const adminValidation = Joi.object({
+  login_id: Joi
+    .string()
+    .required()
+    .custom(async (login_id, helper)=> {
+      let admin = await getAdminCredentials(login_id);
+      if(admin == {}){
+        helper.message("The album seems to not exist")
+        return helper.error("any.invalid")
+      }
+      return login_id;
+    }),
+  pass: Joi
+    .string()
+    .required()
+    .custom(async (pass, helper)=> {
+      let admin = await getAdminCredentials(login_id);
+      if(admin == {} && pass == admin.pass){
+        return admin.pass
+      }
+      helper.error("any.invalid")
+      return helper.message("The album seems to not exist")
     })
 })
 
 exports.validateUser = async (decodedJwt)=>{
-  const validation = await userValidation.validateAsync(decodedJwt);
-  if(validation.error){
-    return false
-  }
-  return true
+  return await userValidation.validateAsync(decodedJwt);
+}
+
+exports.validateAdmin = async (decodedJwt) => { 
+  return await adminValidation.validateAsync(decodedJwt);
 }
