b650da37405e42aafb6013104e01c352d9f59592
xenotil <adrien.mamias@gmail.com>
2024-02-09 11:57:58 +0100

    Add admin scrip and script form

--- public/script/admin.script.js
+++ public/script/admin.script.js
@@ -1,19 +1,14 @@
-// document.addEventListener('DOMContentLoaded', () => {
-    const folderButton = document.querySelector('[data-modal-toggle="folder-creation-modal"]'); // Sélectionnez votre bouton de déclenchement de la modal de création de dossier
+document.addEventListener('DOMContentLoaded', () => {
+    const folderButton = document.getElementById('openButton');
+    const closeButton = document.getElementById('closeButton');
     const folderModal = document.getElementById('folder-creation-modal');
-
+    
     folderButton.addEventListener('click', function() {
         folderModal.classList.remove('hidden'); // Affiche le modal de création de dossier
     });
 
-    folderModal.addEventListener('click', function(event) {
-        if (event.target === this) {
-            folderModal.classList.add('hidden'); // Cache le modal de création de dossier si l'utilisateur clique à l'extérieur de la modal
-        }
+    closeButton.addEventListener('click', function() {
+        folderModal.classList.add('hidden');
     });
 
-    const closeButton = document.getElementById('closeButton');
-    closeButton.addEventListener('click', function(event) {
-        folderModal.classList.add('hidden'); // Cache le modal de création de dossier lorsque le bouton de fermeture est cliqué
-    });
-// });
+});
