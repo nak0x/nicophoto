00115c4ba2f2ba06fcf97ed6731afee34b930c65
AlexBonifacio <148979761+AlexBonifacio@users.noreply.github.com>
2024-02-09 15:40:26 +0100

    test

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const fs = require("fs");
+const path = require("path")
 
 // const routeur = require("./router/index.routes");
+const { initTables, insertAdmin} = require("./database/database_utils");
+
+// Init db
+initTables();
+
+insertAdmin(process.env.ADMIN_LOGIN, process.env.ADMIN_PASS);
+
+const routeur = require("./router/index.routes");
+const internal = require('stream');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,8 +20,8 @@ app.use(express.static("public"));
 
 // app.use("/api", routeur);
 
-app.use("*", async (req, res)=>{
-  res.status(404).sendFile(await fs.readFile("./public/404.html"));
+app.use("*", (req, res)=>{
+  res.status(404).sendFile("/404.html", {root: path.join(__dirname, 'public')});
 })
 
 app.listen(port, () => {
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,16 @@
-//  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/
+ /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/
 const jwt = require("jsonwebtoken");
 const joi = require("joi");
 const { Database } = require("../database/database");
 
+exports.auth = ()=>{return}
 function getAdminById(id){
   try{
     const result = Database.run(`
       SELECT * FROM admins WHERE login_id = ?;
     `, [id]);
+    console.log(result);
   }catch(err){
     return {}
   }
 }
-
-export function 
