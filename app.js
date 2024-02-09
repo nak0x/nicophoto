027528ca01957ed52c6423e4ff9a5198001d246a
AlexBonifacio <148979761+AlexBonifacio@users.noreply.github.com>
2024-02-09 09:47:00 +0100

    coucou

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const fs = require("fs");
 
-const routeur = require("./router/index.routes");
+// const routeur = require("./router/index.routes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.use("/api", routeur);
+// app.use("/api", routeur);
 
 app.use("*", async (req, res)=>{
   res.status(404).sendFile(await fs.readFile("./public/404.html"));
