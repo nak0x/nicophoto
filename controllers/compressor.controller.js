182e1e829e99098a75b3ef51a20012c666862063
Ewan-Reveille <ewan.reveille23@gmail.com>
2024-03-04 09:36:37 +0100

    image compressor to base 64 DONE

--- controllers/compressor.controller.js
+++ controllers/compressor.controller.js
@@ -2,7 +2,7 @@ const sharp = require('sharp');
 const axios = require('axios');
 const fs = require('fs');
 
-const compressImage = async (imageUrl) => {
+exports.compressImage = async(imageUrl) => {
     try {
         const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
         const imageBuffer = Buffer.from(response.data);
@@ -11,11 +11,9 @@ const compressImage = async (imageUrl) => {
             .resize({ height: 728 })
             .toBuffer();
 
-        fs.writeFileSync('imageFinallyCompressed.webp', compressedImageBuffer);
-        console.log('Your image has been successfully compressed.');
+        const base64String = base64.from(compressedImageBuffer);
+        return base64String;
     } catch (error) {
         console.error(error);
     }
 };
-
-compressImage('https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU');
