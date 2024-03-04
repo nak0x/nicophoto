36c61bdf0a70edc3dd3911049ea3789fe9020f90
Baptiste JULLION <contact@baptiste-jullion.dev>
2024-03-04 14:21:01 +0100

    CRUD Image

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -2,7 +2,6 @@ const { v4: uuidv4 } = require("uuid");
 const multer = require("multer");
 const fs = require("fs");
 const Database = require("../database/database");
-const theUid = uuidv4();
 const { compressImage } = require("../controllers/compressor.controller");
 
 const storage = multer.diskStorage({
@@ -10,14 +9,13 @@ const storage = multer.diskStorage({
     cb(null, `albums/${req.params.album_uid}`);
   },
   filename: function (req, file, cb) {
-    cb(null, `${theUid}.${file.originalname.split(".").pop()}`);
+    cb(null, `${uuidv4()}.${file.originalname.split(".").pop()}`);
   },
 });
 
 const upload = multer({ storage: storage });
 
 exports.createImage = (req, res) => {
-  // verify if the folder with the album uid exists
   try {
     if (
       !fs.existsSync(`albums/${req.params.album_uid}`) ||
@@ -30,45 +28,153 @@ exports.createImage = (req, res) => {
     }
 
     upload.single("image")(req, res, async (err) => {
-      if (err) {
-        throw new Error("Error uploading image");
-      }
+      try {
+        if (err) {
+          throw new Error("Error uploading image");
+        }
 
-      const preview = compressImage(req.file.path);
+        const preview = await compressImage(req.file.path);
 
-      const query =
-        "INSERT INTO image (uid, name, preview, pinned, album_uid) VALUES (?, ?, ?, ?, ?)";
-      const params = [
-        theUid,
-        req.body.name,
-        preview,
-        req.body.pinned,
-        req.params.album_uid,
-      ];
+        const mime_type = req.file.mimetype;
 
-      await new Promise((resolve, reject) => {
-        Database.run(query, params, (err) => {
-          if (err) {
-            reject(err);
-          }
-          resolve();
+        const query =
+          "INSERT INTO image (uid, name, preview, pinned, album_uid, mime_type) VALUES (?, ?, ?, ?, ?, ?)";
+        const params = [
+          req.file.filename.split(".").shift(),
+          req.body.name,
+          preview,
+          req.body.pinned,
+          req.params.album_uid,
+          mime_type,
+        ];
+
+        await new Promise((resolve, reject) => {
+          Database.run(query, params, (err) => {
+            if (err) {
+              reject(err);
+            }
+            resolve();
+          });
         });
-      });
 
-      // File uploaded successfully
-      res.send("Image created");
+        res.send("Image created");
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          error: { code: 500, message: error },
+        });
+      }
     });
   } catch (error) {
-    res.send({
+    res.status(500).send({
       success: false,
       error: { code: 500, message: error },
     });
   }
 };
 
-// uid TINYTEXT PRIMARY KEY NOT NULL,
-// name VARCHAR(255) DEFAULT NULL,
-// preview BLOB,
-// pinned BOOLEAN DEFAULT FALSE,
-// album_uid INTEGER NOT NULL,
-// FOREIGN KEY(album_uid) REFERENCES album(uid)
+exports.getImage = async (req, res) => {
+  try {
+    const query = "SELECT * FROM image WHERE uid = ? AND album_uid = ?";
+    const params = [req.params.image_uid, req.params.album_uid];
+
+    const row = await new Promise((resolve, reject) => {
+      Database.get(query, params, (err, row) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(row);
+      });
+    });
+
+    if (!row) {
+      throw new Error("Image not found");
+    }
+
+    res.send({
+      success: true,
+      data: row,
+    });
+  } catch (error) {
+    res.status(404).send({
+      success: false,
+      error: { code: 404, message: error },
+    });
+  }
+};
+
+exports.updateImage = async (req, res) => {
+  try {
+    const body = req.body;
+    for (let [key, value] of Object.entries(body)) {
+      if (value) {
+        const query = `UPDATE image SET ${key} = ? WHERE uid = ?`;
+        const params = [value, req.params.image_uid];
+
+        await new Promise((resolve, reject) => {
+          Database.run(query, params, (err) => {
+            if (err) {
+              reject(err);
+            }
+            resolve();
+          });
+        });
+      }
+    }
+
+    res.send({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    res.status(404).send({
+      success: false,
+      error: { code: 404, message: error },
+    });
+  }
+};
+
+exports.deleteImage = async (req, res) => {
+  try {
+    const querySelect =
+      "SELECT mime_type FROM image WHERE uid = ? AND album_uid = ?";
+    const paramsSelect = [req.params.image_uid, req.params.album_uid];
+
+    const row = await new Promise((resolve, reject) => {
+      Database.get(querySelect, paramsSelect, (err, row) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(row);
+      });
+    });
+
+    const query = "DELETE FROM image WHERE uid = ? AND album_uid = ?";
+    const params = [req.params.image_uid, req.params.album_uid];
+
+    await new Promise((resolve, reject) => {
+      Database.run(query, params, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    });
+
+    fs.unlinkSync(
+      `albums/${req.params.album_uid}/${req.params.image_uid}.${row.mime_type
+        .split("/")
+        .pop()}`
+    );
+
+    res.send({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    res.status(404).send({
+      success: false,
+      error: { code: 404, message: error },
+    });
+  }
+};
--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -2,15 +2,14 @@ const Database = require("../database/database");
 
 exports.getAllAlbumImages = async (req, res) => {
   try {
-    const query = "SELECT * FROM image WHERE album_id = ?";
-    const params = [];
+    const query = "SELECT * FROM image WHERE album_uid = ?";
+    const params = [req.params.album_uid];
 
     const rows = await new Promise((resolve, reject) => {
       Database.all(query, params, (err, rows) => {
         if (err) {
           reject(err);
         }
-        console.log(rows);
         resolve(rows);
       });
     });
