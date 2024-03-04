81d2b45fac65cee3526bf47e53daaaaebbf3dcc3
AlexBonifacio <148979761+AlexBonifacio@users.noreply.github.com>
2024-03-04 16:15:34 +0100

    commit

---
+++ router/api/albums.routes.js
@@ -0,0 +1,9 @@
+const AlbumsRouter = require("express").Router();
+
+const { authToken } = require("../../middlewares/auth.middleware");
+
+const albumsController = require("../../controllers/albums.controller");
+
+AlbumsRouter.route("/").get(/*authToken("admin"),*/ albumsController.getAlbums);
+
+module.exports = AlbumsRouter;
