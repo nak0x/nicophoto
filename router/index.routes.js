1701cb5ecc177ce5c5624a89a240d438d5889145
Nak0_x <pro.theolesage@gmail.com>
2024-03-05 14:48:49 +0100

    Propa auth middleware

--- router/index.routes.js
+++ router/index.routes.js
@@ -1,7 +1,9 @@
 const Router = require("express").Router();
 
-// Importing routes
+// Auth middleware
+const { auth } = require("../middlewares/auth.middleware");
 
+// Importing routes
 const albumRoutes = require("./api/album.routes");
 const albumsRoutes = require("./api/albums.routes");
 const imageRoutes = require("./api/images.routes");
@@ -10,8 +12,7 @@ const authRoutes = require("./api/auth.routes");
 // Routing
 
 Router.use("/auth", authRoutes);
-
-Router.use("/albums", albumsRoutes);
+Router.use("/albums", auth('api', 'admin'), albumsRoutes);
 Router.use("/album", albumRoutes);
 
 module.exports = Router;
--- router/views.routes.js
+++ router/views.routes.js
@@ -1,17 +1,11 @@
 const Router = require("express").Router({mergeParams: true});
 
 const { getAlbumByURL } = require("../controllers/album.controller");
-const auth = require("../middlewares/auth.middleware");
-const AlbumRouter = require("./api/album.routes");
+const { auth } = require("../middlewares/auth.middleware");
 
 const adminRoutes = require("./pages/admin.routes");
 
-// Router.use("/", (req, res) => {
-  // res.render('index');
-// })
-
-Router.use("/admin", /*auth.authToken("admin"),*/ adminRoutes);
-Router.get("/:album_slug", getAlbumByURL);
-// Router.use("/album/:id", /*auth.authToken("album"), */ albumRoutes);
+Router.use("/admin", auth('render', 'admin'), adminRoutes);
+Router.get("/:album_slug", auth('render'), getAlbumByURL);
 
 module.exports = Router;
