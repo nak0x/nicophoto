08cb9749aae6f1c6bd65a35d211f2fe4131c3fa7
Ewan-Reveille <ewan.reveille23@gmail.com>
2024-03-05 11:51:20 +0100

    possibility to get datas from the album slug

--- router/views.routes.js
+++ router/views.routes.js
@@ -1,18 +1,18 @@
-const Router = require("express").Router();
+const Router = require("express").Router({mergeParams: true});
 
+const { getAlbumByURL } = require("../controllers/album.controller");
 const auth = require("../middlewares/auth.middleware");
+const AlbumRouter = require("./api/album.routes");
 
 const adminRoutes = require("./pages/admin.routes");
-const albumRoutes = require("./pages/album.routes");
 
-Router.use("/test", (req, res) => {
-  res.render("test", {
-    name: "Théo",
-    data: ["Test 1", "Test 2", "Test 3"],
-  });
-});
+// Router.use("/", (req, res) => {
+  // res.render('index');
+// })
+
+Router.get("/:album_slug", getAlbumByURL);
 
 Router.use("/admin", /*auth.authToken("admin"),*/ adminRoutes);
-Router.use("/album/:id", /*auth.authToken("album"), */ albumRoutes);
+// Router.use("/album/:id", /*auth.authToken("album"), */ albumRoutes);
 
 module.exports = Router;
