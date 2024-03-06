140d8f35411a863768e8559eece42467ad4a752a
Nak0_x <pro.theolesage@gmail.com>
2024-03-06 15:21:15 +0100

    Add auth to front fetch

--- public/utils.js
+++ public/utils.js
@@ -1,49 +1,53 @@
 async function fetchData(searchUrl, method, data) {
-    let url = '';
-    if (searchUrl.includes('http')) url = searchUrl;
-    else url = BASE_URL + searchUrl;
+  let url = "";
+  if (searchUrl.includes("http")) url = searchUrl;
+  else url = BASE_URL + searchUrl;
 
-    try {
-        const response = await fetch(url, {
-            method,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+      body: JSON.stringify(data),
+    });
 
-        if (!response.ok) {
-            throw new Error('Request failed');
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Erreur:', error.message);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Request failed");
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Erreur:", error.message);
+    throw error;
+  }
 }
 
 async function fetchDataFile(searchUrl, method, data) {
-    let url = '';
-    if (searchUrl.includes('http')) url = searchUrl;
-    else url = BASE_URL + searchUrl;
+  let url = "";
+  if (searchUrl.includes("http")) url = searchUrl;
+  else url = BASE_URL + searchUrl;
 
-    try {
-        const options = {
-            method,
-            body: data
-        };
+  try {
+    const options = {
+      method,
+      body: data,
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    };
 
-        const response = await fetch(url, options);
+    const response = await fetch(url, options);
 
-        if (!response.ok) {
-            throw new Error('Request failed');
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Error:', error.message);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Request failed");
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
 }
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = "http://localhost:8000/api";
